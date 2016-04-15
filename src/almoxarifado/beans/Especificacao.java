3c1a0573bdd4c8eb86c1f59658ad33237dddb10c
cristovao <cristovao@172.16.198.159>
2016-04-14 22:30:38 -0300

    Estrutura inicial do projeto.

---
+++ src/almoxarifado/beans/Especificacao.java
@@ -0,0 +1,54 @@
+package almoxarifado.beans;
+
+public class Especificacao {
+	private String nomeModificador;
+	private String caracteristicasFisicas;
+	private String Observacao;
+	
+	public Especificacao(String nomeModificador, String caracteristicasFisicas, String observacao) {
+		this.nomeModificador = nomeModificador;
+		this.caracteristicasFisicas = caracteristicasFisicas;
+		Observacao = observacao;
+	}
+
+	public String getNomeModificador() {
+		return nomeModificador;
+	}
+
+	public void setNomeModificador(String nomeModificador) {
+		this.nomeModificador = nomeModificador;
+	}
+
+	public String getCaracteristicasFisicas() {
+		return caracteristicasFisicas;
+	}
+
+	public void setCaracteristicasFisicas(String caracteristicasFisicas) {
+		this.caracteristicasFisicas = caracteristicasFisicas;
+	}
+
+	public String getObservacao() {
+		return Observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		Observacao = observacao;
+	}
+
+	/**
+	 * Duas especificações são iguais quando possuem os mesmos nomes modificador.
+	 * 
+	 * @param comparado
+	 * @return
+	 */
+	public boolean equals(Especificacao comparado){
+		boolean resultado = false;
+		
+		if (comparado != null)
+			resultado = this.nomeModificador.equals(comparado.getNomeModificador()) ? 
+					true : false ;
+		
+		return resultado;
+	}
+	
+}
---
+++ src/almoxarifado/beans/Material.java
@@ -0,0 +1,12 @@
+package almoxarifado.beans;
+
+public class Material {
+	private String nomeBasico;
+	private String codigo;
+	private int qtde;
+	private Especificacao esp;
+	private String dataDeAquisicao;
+	
+	
+	
+}
