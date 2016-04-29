0b99692968b4ac580d9f9cdbb4415a6ba5dbe50a
cristovao <cristovao@172.16.198.159>
2016-04-29 19:29:55 -0300

    Implementação correta da fachada de Material. Implementação correta da aplicação para linha de comando do que foi implementado até agora.

--- src/almoxarifado/fachada/FachadaDeMaterial.java
+++ src/almoxarifado/fachada/FachadaDeMaterial.java
@@ -1,122 +1,76 @@
 package almoxarifado.fachada;
 
-import java.util.Scanner;
 import almoxarifado.material.beans.Material;
 import almoxarifado.material.negocio.ControladorDeCadastro;
+import almoxarifado.usuario.beans.Usuario;
 
 public class FachadaDeMaterial {
 	
-	private static int opcao;
-	private static Material mat = null;
-		
-	public static void selecaoDeMenu(Scanner input, ControladorDeCadastro controlCadastro)
-	{
-		do{
-			FachadaDeMaterial.menu();
-			opcao = input.nextInt();
-			input.nextLine();
-			switch(opcao){
-			case 0:
-				System.out.println("<<< Encerrando programa! >>>"); break;
-			case 1:
-				FachadaDeMaterial.inserir( controlCadastro, input); break;
-			case 2:
-				FachadaDeMaterial.buscar(mat, controlCadastro, input); break;
-			case 3:
-				FachadaDeMaterial.remover(controlCadastro, input); break;
-			case 4:
-				FachadaDeMaterial.alterar(controlCadastro, input); break;
-			case 5:
-				controlCadastro.listar(); break;
-			default:
-					System.out.println(">>> [Opcao invalida!] <<<\n");
-			}
-		}while (opcao != 0);
+	private static FachadaDeMaterial instancia;
+	private ControladorDeCadastro controlCadastro;
+	
+	private FachadaDeMaterial(Usuario u){
+		this.controlCadastro = new ControladorDeCadastro(u);
 	}
 	
-	
-	public static void menu()
-	{
-		System.out.println("\n=============================");
-		System.out.println(" al-Shariff Menu Material    ");
-		System.out.println("=============================");
-		System.out.println("\nSelecione a opcao desejada:");
-		System.out.println("[1] - Inserir material");
-		System.out.println("[2] - Buscar material");
-		System.out.println("[3] - Remover material");
-		System.out.println("[4] - Alterar material");
-		System.out.println("[5] - Listar material");
-		System.out.println("[0] - Encerrar programa");
-		System.out.println("\n=============================");
-		System.out.println("@ll rights reserved");
-		System.out.println("---");
-		System.out.println("\nOpcao: ");
+	public static FachadaDeMaterial getInstance(Usuario u){
+		if (instancia == null)
+			instancia = new FachadaDeMaterial(u);
+		return instancia;
 	}
 	
-	public static void inserir(ControladorDeCadastro controlador, Scanner input)
-	{
-		System.out.println("\n--- Inserir material ---");
-		System.out.print("\nCodigo: " + controlador.getCodAutomatico());
-		System.out.print("\nNome básico: ");
-		String nome = input.nextLine();
-		System.out.print("\nData de Aquisicao: ");
-		String data = input.nextLine();
-		System.out.println("\nQuantidade: ");
-		int qtde = input.nextInt();
-		input.nextLine();
-		Material aInserir = new Material(nome, null, qtde, null, data, null);
-		if ( controlador.inserirMaterial(aInserir))
-			System.out.println("\n<<< operação feita com sucesso! >>>");
-		else{
-			System.out.println("\n>>> ERRO! <<<");
-			System.out.println("\n>>> falha na operação <<<");
-		} 
+	/**
+	 * @param cod
+	 * @return
+	 * @see almoxarifado.material.negocio.ControladorDeCadastro#buscaCodMaterial(java.lang.String)
+	 */
+	public Material buscaCodMaterial(String cod) {
+		return controlCadastro.buscaCodMaterial(cod);
 	}
 	
-	public static void buscar(Material mat, ControladorDeCadastro controlador, Scanner input) 
-	{
-		System.out.println("\n--- Buscar material ---");
-		System.out.print("\nCodigo: ");
-		String cod = input.nextLine();
-		System.out.println("\n Resultado de busca: ");
-		if ((mat = controlador.buscaCodMaterial(cod)) != null){
-			System.out.println(mat.toString());
-		}
-		else System.out.println("material não localizado");
+	/**
+	 * @param m
+	 * @return
+	 * @see almoxarifado.material.negocio.ControladorDeCadastro#inserirMaterial(almoxarifado.material.beans.Material)
+	 */
+	public boolean inserirMaterial(Material m) {
+		return controlCadastro.inserirMaterial(m);
+		/// TODO: implementar retorno de exceções aqui
 	}
 	
-	public static void remover(ControladorDeCadastro controlador, Scanner input){
-		System.out.println("\n--- Remover Material ---");
-		System.out.print("\nCodigo: ");
-		String cod = input.nextLine();
-		if (controlador.removerMaterial(cod))
-			System.out.println("\n<<< operação feita com sucesso! >>>");
-		else System.out.println("livro não localizado");
+	/**
+	 * @param m
+	 * @return
+	 * @see almoxarifado.material.negocio.ControladorDeCadastro#atualizarMaterial(almoxarifado.material.beans.Material)
+	 */
+	public boolean atualizarMaterial(Material m) {
+		return controlCadastro.atualizarMaterial(m);
 	}
 	
-	public static void alterar(ControladorDeCadastro controlador, Scanner input){
-		System.out.println("\n--- Alterar Material ---");
-		System.out.print("\nCodigo: ");
-		String cod = input.nextLine();
-		Material mat = controlador.buscaCodMaterial(cod);
-		if (mat != null){
-			System.out.println("Material a ser alterado: ");
-			System.out.println(mat.toString());
-			System.out.print("\nNovo nome basico: ");
-			mat.setNomeBasico(input.nextLine());
-			System.out.print("\nNovo data de aquisicao: ");
-			mat.setDataDeAquisicao(input.nextLine());
-			System.out.println("\nNova quantidade: ");
-			mat.setQtde(input.nextInt());
-			input.nextLine();
-			if(controlador.atualizarMaterial(mat))
-				System.out.println("\n<<< operação feita com sucesso! >>>");
-			else{
-				System.out.println("\n>>> ERRO! <<<");
-				System.out.println("\n>>> falha na operação <<<");
-			}
-		}
-		else System.out.println("material não localizado");
+	/**
+	 * @param cod
+	 * @return
+	 * @see almoxarifado.material.negocio.ControladorDeCadastro#removerMaterial(java.lang.String)
+	 */
+	public boolean removerMaterial(String cod) {
+		return controlCadastro.removerMaterial(cod);
 	}
+	
+	/**
+	 * 
+	 * @see almoxarifado.material.negocio.ControladorDeCadastro#listar()
+	 */
+	public void listar() {
+		controlCadastro.listar();
+	}
+
+	/**
+	 * @return
+	 * @see almoxarifado.material.negocio.ControladorDeCadastro#getCodAutomatico()
+	 */
+	public int getCodAutomatico() {
+		return controlCadastro.getCodAutomatico();
+	}
+	
 
 }
